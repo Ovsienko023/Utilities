b26ceaef61e87c1827f172bdbfbaf7fece2a92d2
Ovsienko023 <Ovsienko023@gmail.com>
2020-05-12 16:40:01 +0300

    mod flask session

--- flask_session.py
+++ flask_session.py
@@ -3,14 +3,16 @@ from flask import Flask, session
 app = Flask(__name__)
 app.secret_key = 'app secret key'
 
-@app.route('/')
-def index():
+@app.route('/<login>')
+def index(login):
     if 'counter' in session:
-        print(session)
-        session['counter'] += 1
+            session['counter'] += 1
     else:
         session['counter'] = 1
-    return 'Counter: ' + str(session['counter'])
+    
+    # session.pop('counter', None)
+    
+    return {"st": session['counter']}
 
 
 app.run(host='192.168.16.70', port=5555)
@@ -19,13 +21,17 @@ app.run(host='192.168.16.70', port=5555)
 #### Client test
 
 def url_session():
-    url = r'http://192.168.16.70:5555/'
-    data = {"1":"wqdw"}
+    url = r'http://192.168.16.70:5555/vik'
+    
     a = requests.get(url)
-    print(a.content)
+    print(a.content, '!')
     cookies = a.cookies
-
+    
     b = requests.get(url, cookies=cookies)
-    print(b.content)
+    print(b.content, '!!')
+    cookies = b.cookies
+
+    c = requests.get(url, cookies=cookies)
+    print(c.content, '!!!')
 
 # url_session()
