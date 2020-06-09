7c8c5a20dfb050f0c7c67d1436019f7ea9e65baf
Ovsienko023 <Ovsienko023@gmail.com>
2020-06-09 13:16:39 +0300

    mod en_ru and ru_end

--- translate.py
+++ translate.py
@@ -1,10 +1,19 @@
 import requests
 
 
-def translate(word):
+def translate_en_ru(word):
     url = f'https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ru&dt=t&q={word}'
     word_lst = requests.post(url)
     translate_word = word_lst.json()
     return translate_word[0][0][0]
 
-translate('dog')
+
+def translate_ru_en(word):
+    url = f'https://translate.googleapis.com/translate_a/single?client=gtx&sl=ru&tl=en&dt=t&q={word}'
+    word_lst = requests.post(url)
+    translate_word = word_lst.json()
+    return translate_word[0][0][0]
+
+
+print(translate_en_ru('dog'))
+print(translate_ru_en('собака'))
