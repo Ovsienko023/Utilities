8fe9c70af5928812e48a2e7eb244f48a3f974e70
ovsienko <ovsienko@trueconf.ru>
2020-10-21 11:02:04 +0300

    added castom errors

--- cerberus.py
+++ cerberus.py
@@ -1,10 +1,34 @@
 import cerberus
+from cerberus.errors import BasicErrorHandler
 
 
-{'min': 10.1, 'max': 10.9}  # что бы сработало, 'lang_id' обязательно болжно быть int()
-{'minlength': 1, 'maxlength': 3}
-{'nullable': True}  # значение ключа может быть None
-{'regex': r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'}  # Для email
+"""
+    {field} - где произошла ошибка,
+    {value} - невалидное значение,
+    {constraint} - то что должно быть(указанно в схеме)
+    ---------------------------------------------------------------------------------------
+    {'min': 10.1, 'max': 10.9}  # что бы сработало, 'lang_id' обязательно болжно быть int()
+    {'minlength': 1, 'maxlength': 3}
+    {'nullable': True}  # значение ключа может быть None
+    {'regex': r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'}  # Для email
+
+""" 
+
+
+class CustomErrorHandler(BasicErrorHandler):
+    messages = BasicErrorHandler.messages.copy()
+
+    messages[0x02] = "нет обязательного поля!"
+
+    messages[0x24] = 'значение должно быть типа {constraint} как в схеме '
+
+    messages[0x41] = "для регялярки на email"
+    messages[0x42] = "меньше мin {constraint}"
+    messages[0x43] = "привышен мах {constraint}"
+    messages[0x44] = "значение должно быть этим словом: {constraint}"
+    
+    messages[0x61] = "Ошибка приведения к типу {constraint} (у нас int())"
+
 
 schema = {
     'lang_id': {'required': True, 'regex': r'[1-9]'},
@@ -14,7 +38,7 @@ schema = {
     'email': {'type': 'string', 'regex': r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'}    
 }
 
-validator = cerberus.Validator(schema)
+validator = cerberus.Validator(schema, error_handler=CustomErrorHandler)
 validator.allow_unknown = True  # Для разрешения ключей которых нет в схеме
 
 
