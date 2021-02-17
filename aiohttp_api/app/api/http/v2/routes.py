c78e37c460df3e61c5fb4f9f6c6f5619f493df6d
ovsienko <ovsienko@trueconf.ru>
2021-02-17 17:16:45 +0300

    del trash

--- aiohttp_api/app/api/http/v2/routes.py
+++
@@ -1,8 +0,0 @@
-import app.api.http.v1.handlers.langs_time as misc
-from aiohttp import web
-
-
-def setup_routes(app):
-    app.add_routes([
-        web.get('/misc/langs', misc.get_langs)
-    ])
--- aiohttp_api/app/api/http/v2/validation.py
+++
@@ -1,8 +0,0 @@
-
-schema = {
-    'lang_id': {'required': True, 'regex': r'[1-9]'},
-    'time_zone_id': {'required': True, 'regex': r'[1-9]'},
-    'name': {'type': 'string', 'nullable': True},
-    'display_name': {'type': 'string'},
-    'email': {'type': 'string', 'regex': r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'}    
-}
