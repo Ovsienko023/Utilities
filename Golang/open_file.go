ca0d99ed335362d80aa35ffc50c01743d23a5d77
ovsienko <ovsienko@trueconf.ru>
2021-05-13 21:19:00 +0300

    example open file and convert to float64

---
+++ Golang/open_file.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("tex.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	scaner := bufio.NewScanner(file)
+	for scaner.Scan() {
+		// fmt.Println(scaner.Text())
+		// fmt.Println(reflect.TypeOf(scaner.Text()))
+		value, err := strconv.ParseFloat(scaner.Text(), 64)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(value)
+	}
+
+}
